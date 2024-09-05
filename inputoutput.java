abd50d01ecd12f4693a51e05b8c796b75a49728f
shivam240104 <mauryatanmay845@gmail.com>
2024-09-05 15:33:53 +0530

    concatenation in java string

--- inputoutput.java
+++ inputoutput.java
@@ -3,12 +3,17 @@ import java.util.*;
 public class inputoutput {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String add = sc.nextLine();
-        System.out.println(add);
+        // Scanner sc = new Scanner(System.in);
+        // String add = sc.nextLine();
+        // System.out.println(add);
 
-        // string length
+        // // string length
 
-        System.out.println(add.length());
+        // System.out.println(add.length());
+
+        String fn= "Tanmay";
+        String ln = "Maurya";
+        String fulln= fn + " " + ln ;
+        System.out.println(fulln);
     }
 }
