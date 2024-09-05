8ae5942101f6cd8143a7811c7ef5fe2a5e0e26e4
shivam240104 <mauryatanmay845@gmail.com>
2024-09-05 15:19:46 +0530

    input/output in string

---
+++ inputoutput.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class inputoutput {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String add = sc.nextLine();
+        System.out.println(add);
+    }
+}
